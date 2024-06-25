6b08b905465939999168c721d675b58dc93ef606
Strabatras <strabatras@gmail.com>
2024-06-25 20:32:56 +0300

    dependencies added

--- src/main/java/ru/dedus/ground/Application.java
+++ src/main/java/ru/dedus/ground/Application.java
@@ -1,13 +1,13 @@
 package ru.dedus.ground;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroundApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(GroundApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
